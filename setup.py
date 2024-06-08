a713ef8f6be40a13b4e41093e6f9cde2675567f4
Johan Isaksson <johan@izaxon.com>
2024-06-08 12:23:19 +0200

    fix 2

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='codicent-py',
     version='1.0',
-    pv_packages=['codicent'],
+    pv_modules=['codicentpy'],
     install_requires=['requests'],
 )
