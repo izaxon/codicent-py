95aad2350cfd306b8655de47d2b90613b0399e34
Johan Isaksson <johan@izaxon.com>
2024-06-10 13:42:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='codicent-py',
     version='1.0',
     package_dir={'': 'src'},
-    packages=['codicentpy'],
+    packages=[''],  # Empty string to include the root package
+    py_modules=['codicentpy'],  # Specify the codicentpy module
     install_requires=['requests'],
 )
