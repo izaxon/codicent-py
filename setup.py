adffdc862f52c967d8c8813c8881ce1aa9532e17
Johan Isaksson <johan@izaxon.com>
2024-06-10 13:39:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='codicent-py',
     version='1.0',
-    packages=['src'],
+    package_dir={'': 'src'},
+    packages=['codicentpy'],
     install_requires=['requests'],
 )
